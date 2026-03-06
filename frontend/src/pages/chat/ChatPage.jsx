ca1000611f765c336a546bd4a4d857c5974f0889
kencrimsonIT <tongkien930@gmail.com>
2026-03-06 16:47:00 +0700

    feat (password-recovery): Giao diện trang chat

---
+++ frontend/src/pages/chat/ChatPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./ChatPage.scss";
+
+const ChatPage = () => {
+    return (
+        <div className="container">
+            <div className=""></div>
+        </div>
+    )
+}
+
+export default ChatPage;
