497481c39a511f1ae15e998e1a3584681f396185
kencrimsonIT <tongkien930@gmail.com>
2026-03-06 16:46:44 +0700

    feat (password-recovery): Giao diện trang profile

---
+++ frontend/src/pages/profile/ProfilePage.jsx
@@ -0,0 +1,11 @@
+import React, {useState} from "react";
+import "./ProfilePage.scss";
+import {PenLine, Camera, Info} from "lucide-react";
+
+const ProfilePage = () => {
+    return (
+        <div></div>
+    );
+}
+
+export default ProfilePage;
