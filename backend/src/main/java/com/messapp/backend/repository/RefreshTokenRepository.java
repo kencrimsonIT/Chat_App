0bf73ce765a0de2d12f9bb8db098d8540b176672
kencrimsonIT <tongkien930@gmail.com>
2026-04-22 22:27:52 +0700

    feat (repository): Tạo repository cho các entity User, Role, RefreshToken

--- backend/src/main/java/com/messapp/backend/repository/RefreshTokenRepository.java
+++ backend/src/main/java/com/messapp/backend/repository/RefreshTokenRepository.java
@@ -1,4 +1,15 @@
 package com.messapp.backend.repository;
 
-public interface RefreshTokenRepository {
+import com.messapp.backend.entity.RefreshToken;
+import com.messapp.backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByToken(String token);
+    Optional<RefreshToken> findByUser(User user);
+    void deleteByUser(User user);
 }
--- backend/src/main/java/com/messapp/backend/repository/UserRepository.java
+++ backend/src/main/java/com/messapp/backend/repository/UserRepository.java
@@ -1,4 +1,16 @@
 package com.messapp.backend.repository;
 
-public interface UserRepository {
+import com.messapp.backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+
+    boolean existsByUsername(String username); //username đã tồn tại hay chưa
+    boolean existsByEmail(String email); //email đã tồn tại hay chưa
 }
