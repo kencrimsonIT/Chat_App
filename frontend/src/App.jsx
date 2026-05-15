e4e5b8c79f05dc5c05954584e3cd0050225d6229
kencrimsonIT <tongkien930@gmail.com>
2026-05-15 11:26:47 +0700

    feat (main-app): Cấu hình Route cho trang chat và dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,8 @@ import ForgotPasswordPage from "./pages/password-recovery/ForgotPasswordPage";
 import RecoveryPasswordPage from "./pages/password-recovery/RecoveryPasswordPage";
 import ProfilePage from "./pages/profile/ProfilePage";
 import "./App.scss";
+import ChatPage from "./pages/chat/ChatPage";
+import DashboardPage from "./pages/admin/dashboard/DashboardPage";
 
 const App = () => {
   return (
@@ -15,6 +17,12 @@ const App = () => {
             <Route path="/forgot-password" element={<ForgotPasswordPage />} />
             <Route path="/reset-password/:token" element={<RecoveryPasswordPage />} />
             <Route path="/profile" element={<ProfilePage />} />
+
+            {/*Client side*/}
+            <Route path="/chat" element={<ChatPage />} />
+
+            {/*Admin side*/}
+            <Route path="/dashboard" element={<DashboardPage />} />
         </Routes>
     </Router>
   );
