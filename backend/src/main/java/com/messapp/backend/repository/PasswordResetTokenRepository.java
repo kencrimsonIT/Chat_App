663a5811a8c5ef7d5b0b21a16e82ccbaacd74ffc
kencrimsonIT <tongkien930@gmail.com>
2026-05-24 00:44:28 +0700

    feat (repository): Tạo repository cho entity PasswordResetToken

---
+++ backend/src/main/java/com/messapp/backend/repository/PasswordResetTokenRepository.java
@@ -0,0 +1,12 @@
+package com.messapp.backend.repository;
+
+import com.messapp.backend.entity.PasswordResetToken;
+import com.messapp.backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
+    Optional<PasswordResetToken> findByToken(String token);
+    void deleteByUser(User user);
+}
