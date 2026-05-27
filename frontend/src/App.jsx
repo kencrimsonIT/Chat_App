9bd2d3c3f0ec1fbd8b17a04857170cf9c74aab0a
kencrimsonIT <tongkien930@gmail.com>
2026-05-28 01:36:37 +0700

    feat (main-app): Thêm cấu hình OAuthProvider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import LoginPage from "./pages/login/LoginPage";
 import RegisterPage from "./pages/register/RegisterPage";
 import ForgotPasswordPage from "./pages/password-recovery/ForgotPasswordPage";
@@ -15,25 +16,27 @@ const App = () => {
   const darkMode = useSelector((state) => state.theme.darkMode);
 
   return (
-    <div className={`app-container ${darkMode ? "dark" : "light"}`}>
-      <Router>
-        <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-          <Route path="/recovery-password" element={<RecoveryPasswordPage />} />
-          <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/change-password" element={<ChangePasswordPage />} />
-          <Route path="/" element={<HomePage />} />
+    <GoogleOAuthProvider clientId="399351923550-0rbhpo190v1adbibh2tesmahd38v6f9p.apps.googleusercontent.com">
+      <div className={`app-container ${darkMode ? "dark" : "light"}`}>
+        <Router>
+          <Routes>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+            <Route path="/recovery-password" element={<RecoveryPasswordPage />} />
+            <Route path="/profile" element={<ProfilePage />} />
+            <Route path="/change-password" element={<ChangePasswordPage />} />
+            <Route path="/" element={<HomePage />} />
 
-          {/*Client side*/}
-          <Route path="/chat" element={<ChatPage />} />
+            {/*Client side*/}
+            <Route path="/chat" element={<ChatPage />} />
 
-          {/*Admin side*/}
-          <Route path="/dashboard" element={<DashboardPage />} />
-        </Routes>
-      </Router>
-    </div>
+            {/*Admin side*/}
+            <Route path="/dashboard" element={<DashboardPage />} />
+          </Routes>
+        </Router>
+      </div>
+    </GoogleOAuthProvider>
   );
 }
 
