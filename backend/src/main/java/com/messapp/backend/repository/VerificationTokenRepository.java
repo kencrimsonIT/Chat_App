b13b4f111c31046e67f8b895868be8e917eaf2d0
kencrimsonIT <tongkien930@gmail.com>
2026-06-03 23:17:00 +0700

    feat (repository): Tạo repository cho VerificationToken

---
+++ backend/src/main/java/com/messapp/backend/repository/VerificationTokenRepository.java
@@ -0,0 +1,14 @@
+package com.messapp.backend.repository;
+
+import com.messapp.backend.entity.User;
+import com.messapp.backend.entity.VerificationToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long> {
+    Optional<VerificationToken> findByToken(String token);
+    void deleteByUser(User user);
+}
