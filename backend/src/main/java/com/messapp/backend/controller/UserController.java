5cadeeea7e44c085694dd7e20ffdb4783180e96e
kencrimsonIT <tongkien930@gmail.com>
2026-06-07 21:39:53 +0700

    feat (controller): Xử lý controller cho profile người dùng

---
+++ backend/src/main/java/com/messapp/backend/controller/UserController.java
@@ -0,0 +1,40 @@
+package com.messapp.backend.controller;
+
+import com.messapp.backend.entity.User;
+import com.messapp.backend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/upload-avatar")
+    public ResponseEntity<String> uploadAvatar(@RequestParam("file") MultipartFile file) throws IOException {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = userService.updateAvatar(authentication.getName(), file);
+        return ResponseEntity.ok(user.getAvatarUrl());
+    }
+
+    @PostMapping("/upload-cover")
+    public ResponseEntity<String> uploadCover(@RequestParam("file") MultipartFile file) throws IOException {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = userService.updateCover(authentication.getName(), file);
+        return ResponseEntity.ok(user.getCoverUrl());
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<User> getCurrentUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = userService.getUserByUsername(authentication.getName());
+        return ResponseEntity.ok(user);
+    }
+}
