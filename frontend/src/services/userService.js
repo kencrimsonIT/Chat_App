6e8b9558ecd2e2825f718aec51f389b77928d244
kencrimsonIT <tongkien930@gmail.com>
2026-06-07 21:42:10 +0700

    feat (service): Fetch API xử lý trang profile người dùng

---
+++ frontend/src/services/userService.js
@@ -0,0 +1,36 @@
+import axiosInstance from "./axios_instance";
+
+const uploadAvatar = async (file) => {
+    const formData = new FormData();
+    formData.append("file", file);
+    const response = await axiosInstance.post("/users/upload-avatar", formData, {
+        headers: {
+            "Content-Type": "multipart/form-data",
+        },
+    });
+    return response.data;
+};
+
+const uploadCover = async (file) => {
+    const formData = new FormData();
+    formData.append("file", file);
+    const response = await axiosInstance.post("/users/upload-cover", formData, {
+        headers: {
+            "Content-Type": "multipart/form-data",
+        },
+    });
+    return response.data;
+};
+
+const getCurrentUser = async () => {
+    const response = await axiosInstance.get("/users/me");
+    return response.data;
+};
+
+const userService = {
+    uploadAvatar,
+    uploadCover,
+    getCurrentUser,
+};
+
+export default userService;
