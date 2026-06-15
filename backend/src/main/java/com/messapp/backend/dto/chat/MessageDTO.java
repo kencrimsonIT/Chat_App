b4ab605fa37c53971eb7e2ce7dc24a20a82ce8f6
kencrimsonIT <tongkien930@gmail.com>
2026-06-16 01:48:57 +0700

    feat (DTO): Tạo DTO cho phòng chat

---
+++ backend/src/main/java/com/messapp/backend/dto/chat/MessageDTO.java
@@ -0,0 +1,22 @@
+package com.messapp.backend.dto.chat;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class MessageDTO {
+    private String id;
+    private Long roomId;
+    private Long senderId;
+    private String senderUsername;
+    private String content;
+    private String type;
+    private LocalDateTime createdAt;
+}
