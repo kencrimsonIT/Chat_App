b0c7e47a3d888553776419fe096a685acf8b3545
kencrimsonIT <tongkien930@gmail.com>
2026-06-25 22:40:37 +0700

    feat (repository): Thêm hàm xử lý query tìm user theo từ khóa

--- backend/src/main/java/com/messapp/backend/repository/UserRepository.java
+++ backend/src/main/java/com/messapp/backend/repository/UserRepository.java
@@ -2,8 +2,11 @@ package com.messapp.backend.repository;
 
 import com.messapp.backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -13,4 +16,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsByUsername(String username); //username đã tồn tại hay chưa
     boolean existsByEmail(String email); //email đã tồn tại hay chưa
+
+    // Tìm user khác theo từ khóa (username/email)
+    @Query("SELECT u FROM User u WHERE LOWER(u.username) LIKE LOWER(CONCAT('%', :keyword, '%')) OR LOWER(u.email) LIKE LOWER(CONCAT('%', :keyword, '%'))")
+    List<User> searchUsers(@Param("keyword") String keyword);
 }
