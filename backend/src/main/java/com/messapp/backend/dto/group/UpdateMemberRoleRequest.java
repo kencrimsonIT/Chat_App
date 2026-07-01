6df4be885b6682e617e274b7fad00f93cb7420da
kencrimsonIT <tongkien930@gmail.com>
2026-07-01 14:33:03 +0700

    feat (dto): Tạo DTO gán quyền thành viên nhóm chat

---
+++ backend/src/main/java/com/messapp/backend/dto/group/UpdateMemberRoleRequest.java
@@ -0,0 +1,14 @@
+package com.messapp.backend.dto.group;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UpdateMemberRoleRequest {
+    private String role;
+}
